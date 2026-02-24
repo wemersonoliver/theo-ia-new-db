51bace2b530e35444f587370fc80e325b7f83c56
wemersonoliver <wemerson.tw@gmail.com>
2026-02-24 11:39:23 -0300

    deletei tudo
    deletei tudo

--- src/pages/Index.tsx
+++
@@ -1,7 +0,0 @@
-// Update this page (the content is just a fallback if you fail to update the page)
-
-const Index = () => {
-  return <div className="min-h-screen bg-background" />;
-};
-
-export default Index;
